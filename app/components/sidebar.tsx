4620eae6b2be2f6c7f2c12f431cfc8a428d5f9c6
mxrkai <mnatad314@hotmail.com>
2024-08-03 11:29:26 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -223,8 +223,8 @@ export function SideBar(props: { className?: string }) {
       {...props}
     >
       <SideBarHeader
-        title="NextChat"
-        subTitle="Build your own AI assistant."
+        title="MxrkAI"
+        subTitle="Free gpt-4o-mini."
         logo={<ChatGptIcon />}
       >
         <div className={styles["sidebar-header-bar"]}>
